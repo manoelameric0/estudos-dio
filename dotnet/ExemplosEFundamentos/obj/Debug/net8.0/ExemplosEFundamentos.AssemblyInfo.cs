86453451d5d7bba6674aa24e8eb7949b5f10688e
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2024-04-29 12:14:07 -0300

    Estudos dia 29

--- dotnet/ExemplosEFundamentos/obj/Debug/net8.0/ExemplosEFundamentos.AssemblyInfo.cs
+++ dotnet/ExemplosEFundamentos/obj/Debug/net8.0/ExemplosEFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemplosEFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103c659c6a5064b65d1779be43a7847a6d9b3eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+392ec6811dafad4219dd911e803705766014cf60")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemplosEFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemplosEFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- dotnet/SintaxeEIdentacao/Program.cs
+++ dotnet/SintaxeEIdentacao/Program.cs
@@ -1,9 +1,121 @@
 ﻿using SintaxeEIdentacao.Common.Models;
 
-Pessoa pessoa = new Pessoa();
-pessoa.Nome = "Manoel";
-pessoa.Idade = 19;
-pessoa.Apresentar();
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("RJ");
+
+Console.WriteLine($"Itens na minha lista : {listaString.Count} - Capacidade {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista : {listaString.Count} - Capacidade {listaString.Capacity}");
+
+listaString.Remove("MG");
+
+Console.WriteLine($"Itens na minha lista : {listaString.Count} - Capacidade {listaString.Capacity}");
+
+// Console.WriteLine("Percorrendo a Lista com o FOR");
+// for (int contador = 0; contador < listaString.Count; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} - {listaString[contador]}");
+// }
+
+// Console.WriteLine("Percorrendo a Lista com o FOREACH");
+// int contadorForeach = 0;
+// foreach (string item in listaString)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {item}");
+//     contadorForeach++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int[] arrayInteiros = new int [4];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
+
+// int[] arrayInteirosDobrado = new int [arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+
+
+
+// //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+// Console.WriteLine("Percorrendo o Array com o FOR");
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+// int contadorForreach = 0;
+// foreach (int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForreach} - {valor}");
+//     contadorForreach++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa pessoa = new Pessoa();
+// pessoa.Nome = "Manoel";
+// pessoa.Idade = 19;
+// pessoa.Apresentar();
 
 
 
